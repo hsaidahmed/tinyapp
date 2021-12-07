9aad29424834da1cb70325c136609f81b35111db
hsaidahmed <hashim.saidahmed@gmail.com>
2021-12-07 02:54:25 +0000

    getting post requests ready

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs")
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,11 +21,11 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello<b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello<b>World</b></body></html>\n");
+// });
 // app.get("/set", (req, res) => {
-//   const a = 1;
+  //   const a = 1;
 //   res.send(`a = ${a}`);
 //  });
  
@@ -42,3 +44,6 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 
 }) 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
